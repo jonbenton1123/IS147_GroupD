ffbd797e32b5e294972c333c38ab55a3581472f3
ilyanski <iliqnslavov@gmail.com>
2025-11-17 20:42:34 -0500

    I added file

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"HelloWorld"},{"p":"<Unnamed>","l":"iliyan"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"PasswordValidator"},{"p":"<Unnamed>","l":"shiva"}];updateSearchResults();
