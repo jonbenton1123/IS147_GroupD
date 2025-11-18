dc209f3c4e28366d693e24e5c09a2fe96674ce9f
ilyanski <iliqnslavov@gmail.com>
2025-11-17 20:43:33 -0500

    I added file

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ChargingStation"},{"p":"<Unnamed>","l":"CharginStationStatus"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"ParkingLot"},{"p":"<Unnamed>","l":"User_Authentication"}];updateSearchResults();
